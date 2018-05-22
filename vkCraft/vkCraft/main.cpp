0ed34b61f8d3569c87eef2e51a8752fbb4675204
Tentone <tentone@outlook.com>
2018-05-22 22:45:34 +0100

    Validation layers

--- vkCraft/vkCraft/main.cpp
+++ vkCraft/vkCraft/main.cpp
@@ -8,6 +8,66 @@
 #include <iostream>
 #include <vector>
 
+const std::vector<const char*> validationLayers =
+{
+	"VK_LAYER_LUNARG_standard_validation"
+};
+
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif
+
+//Check if validation layers are supported
+bool checkValidationLayerSupport()
+{
+	uint32_t layerCount;
+	vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+	std::vector<VkLayerProperties> availableLayers(layerCount);
+	vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+	for(const char* layerName : validationLayers)
+	{
+		bool layerFound = false;
+
+		for(const auto& layerProperties : availableLayers)
+		{
+			if (strcmp(layerName, layerProperties.layerName) == 0)
+			{
+				layerFound = true;
+				break;
+			}
+		}
+
+		if (!layerFound)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+//Get required extensions if validation layers are active also add debug extension to the list
+std::vector<const char*> getRequiredExtensions()
+{
+	uint32_t glfwExtensionCount = 0;
+	const char** glfwExtensions;
+	glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+	std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+	if(enableValidationLayers)
+	{
+		extensions.push_back(VK_EXT_DEBUG_REPORT_EXTENSION_NAME);
+	}
+
+	return extensions;
+}
+
+//Main class
 class VkCraft
 {
 public:
@@ -15,11 +75,11 @@ public:
 	{
 		initWindow();
 		initVulkan();
-		//mainLoop();
+		mainLoop();
 		cleanup();
 	}
 private:
-	GLFWwindow * window;
+	GLFWwindow *window;
 	VkInstance instance;
 	
 	//Initialize GLFW window
@@ -40,6 +100,11 @@ private:
 	//Create a new vulkan instance
 	void createInstance()
 	{
+		if(enableValidationLayers && !checkValidationLayerSupport())
+		{
+			throw std::runtime_error("validation layers requested, but not available!");
+		}
+
 		VkApplicationInfo appInfo = {};
 		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
 		appInfo.pApplicationName = "VkCraft";
@@ -54,23 +119,46 @@ private:
 
 		uint32_t glfwExtensionCount = 0;
 		const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-		createInfo.enabledExtensionCount = glfwExtensionCount;
-		createInfo.ppEnabledExtensionNames = glfwExtensions;
+		std::vector<const char*> extensions = getRequiredExtensions();
+		createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+		createInfo.ppEnabledExtensionNames = extensions.data();
+
+		//Check extensions available
+		std::cout << "GLFW Extensions:" << std::endl;
+		for (uint32_t i = 0; i < extensions.size(); i++)
+		{
+			std::cout << extensions[i] << std::endl;
+		}
+
+		if (enableValidationLayers)
+		{
+			createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+			createInfo.ppEnabledLayerNames = validationLayers.data();
+		}
+		else
+		{
+			createInfo.enabledLayerCount = 0;
+		}
 
 		if(vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
 		{
 			throw std::runtime_error("Failed to create vulkan instance!");
 		}
-		
+
+		/*
 		//Check extensions available
 		uint32_t extensionCount = 0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
 		std::vector<VkExtensionProperties> extensions(extensionCount);
 		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
-
+		
+		std::cout << "Available extensions:" << std::endl;
 		for(uint32_t i = 0; i < extensions.size(); i++)
 		{
-			std::cout << "\n" << extensions[i].extensionName << std::endl;
+			std::cout << extensions[i].extensionName << std::endl;
 		}
+		*/
 	}
 
 	//Logic loop
