bc98ca080c0917986e64e11ed6ce6c8651da7b1e
Tentone <tentone@outlook.com>
2018-05-24 19:18:24 +0100

    Working...for some fucking reason

--- vkCraft/vkCraft/main.cpp
+++ vkCraft/vkCraft/main.cpp
@@ -104,7 +104,7 @@ private:
 
 		//Pick a device
 		pickPhysicalDevice();
-		//createLogicalDevice();
+		createLogicalDevice();
 	}
 	
 	//Logic loop
@@ -330,23 +330,23 @@ private:
 		int i = 0;
 		for(const auto& queueFamily : queueFamilies)
 		{
-			if(queueFamily.queueCount > 0 && queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT)
+            if(queueFamily.queueCount > 0 && queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT)
 			{
-				indices.graphicsFamily = i;
-			}
+                indices.graphicsFamily = i;
+            }
 
-			VkBool32 presentSupport = false;
-			vkGetPhysicalDeviceSurfaceSupportKHR(device, i, surface, &presentSupport);
+            VkBool32 presentSupport = false;
+            vkGetPhysicalDeviceSurfaceSupportKHR(device, i, surface, &presentSupport);
 
-			if(queueFamily.queueCount > 0 && presentSupport)
+            if(queueFamily.queueCount > 0 && presentSupport)
 			{
-				indices.graphicsFamily = i;
-			}
+                indices.presentFamily = i;
+            }
 
-			if(indices.isComplete())
+            if(indices.isComplete())
 			{
-				break;
-			}
+                break;
+            }
 
 			i++;
 		}
