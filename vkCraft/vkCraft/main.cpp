d7786836d4ac9740cc72be1c2adffbd302b5cc83
Tentone <tentone@outlook.com>
2018-05-24 18:26:33 +0100

    Derp

--- vkCraft/vkCraft/main.cpp
+++ vkCraft/vkCraft/main.cpp
@@ -1,12 +1,11 @@
 #define GLFW_INCLUDE_VULKAN
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-
 #include <GLFW/glfw3.h>
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
+
 #include <iostream>
+#include <stdexcept>
 #include <vector>
+#include <cstring>
+#include <cstdlib>
 #include <set>
 
 const std::vector<const char*> validationLayers =
@@ -70,13 +69,17 @@ public:
 	}
 private:
 	GLFWwindow *window;
+
 	VkInstance instance;
 	VkDebugReportCallbackEXT callback;
+	VkSurfaceKHR surface;
+
 	VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
 	VkDevice device;
+
 	VkQueue graphicsQueue;
-	VkSurfaceKHR surface;
 	VkQueue presentQueue;
+
 
 	//Initialize GLFW window
 	void initWindow()
@@ -118,7 +121,7 @@ private:
 	{
 		if(enableValidationLayers && !checkValidationLayerSupport())
 		{
-			throw std::runtime_error("validation layers requested, but not available!");
+			throw std::runtime_error("Validation layers requested, but not available!");
 		}
 
 		VkApplicationInfo appInfo = {};
@@ -133,8 +136,6 @@ private:
 		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 		createInfo.pApplicationInfo = &appInfo;
 
-		uint32_t glfwExtensionCount = 0;
-		const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
 		std::vector<const char*> extensions = getRequiredExtensions();
 		createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
 		createInfo.ppEnabledExtensionNames = extensions.data();
@@ -281,7 +282,6 @@ private:
 		createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
 		createInfo.queueCreateInfoCount = static_cast<uint32_t>(queueCreateInfos.size());
 		createInfo.pQueueCreateInfos = queueCreateInfos.data();
-
 		createInfo.pEnabledFeatures = &deviceFeatures;
 		createInfo.enabledExtensionCount = 0;
 
@@ -332,7 +332,7 @@ private:
 
 		std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
 
-		if (enableValidationLayers)
+		if(enableValidationLayers)
 		{
 			extensions.push_back(VK_EXT_DEBUG_REPORT_EXTENSION_NAME);
 		}
@@ -354,24 +354,18 @@ private:
 	{
 		std::cout << "Cleanup" << std::endl;
 
-		//Device
 		vkDestroyDevice(device, nullptr);
-		
-		//Window surface
-		vkDestroySurfaceKHR(instance, surface, nullptr);
 
-		//Vulkan instance
-		vkDestroyInstance(instance, nullptr);
-
-		//Window
-		glfwDestroyWindow(window);
-		glfwTerminate();
-
-		//If validation layers are enabled destroy debug callback
-		if(enableValidationLayers)
+		if (enableValidationLayers)
 		{
 			DestroyDebugReportCallbackEXT(instance, callback, nullptr);
 		}
+
+		vkDestroySurfaceKHR(instance, surface, nullptr);
+		vkDestroyInstance(instance, nullptr);
+
+		glfwDestroyWindow(window);
+		glfwTerminate();
 	}
 
 	//Check if validation layers are supported
@@ -383,11 +377,11 @@ private:
 		std::vector<VkLayerProperties> availableLayers(layerCount);
 		vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
 
-		for (const char* layerName : validationLayers)
+		for(const char* layerName : validationLayers)
 		{
 			bool layerFound = false;
 
-			for (const auto& layerProperties : availableLayers)
+			for(const auto& layerProperties : availableLayers)
 			{
 				if (strcmp(layerName, layerProperties.layerName) == 0)
 				{
@@ -396,7 +390,7 @@ private:
 				}
 			}
 
-			if (!layerFound)
+			if(!layerFound)
 			{
 				return false;
 			}
