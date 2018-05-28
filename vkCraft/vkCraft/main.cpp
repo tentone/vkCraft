099a41c56cb2cd59ad00edf255fc45eb6c9c421d
Tentone <tentone@outlook.com>
2018-05-28 08:36:14 +0100

    Read shader files

--- vkCraft/vkCraft/main.cpp
+++ vkCraft/vkCraft/main.cpp
@@ -8,6 +8,7 @@
 #include <cstring>
 #include <cstdlib>
 #include <set>
+#include <fstream>
 
 const std::vector<const char*> validationLayers =
 {
@@ -418,9 +419,31 @@ private:
 		}
 	}
 
+	//Initializer graphics pipeline, load shaders
 	void createGraphicsPipeline()
 	{
+		auto vertShaderCode = readFile("shaders/vert.spv");
+		auto fragShaderCode = readFile("shaders/frag.spv");
+
 		//TODO <ADD CODE HERE>
+	}
+
+	//Create a shader from SPIR-V code
+	VkShaderModule createShaderModule(const std::vector<char>& code)
+	{
+		VkShaderModuleCreateInfo createInfo = {};
+		createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+		createInfo.codeSize = code.size();
+		createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+
+		VkShaderModule shaderModule;
+
+		if(vkCreateShaderModule(device, &createInfo, nullptr, &shaderModule) != VK_SUCCESS)
+		{
+			throw std::runtime_error("failed to create shader module!");
+		}
+
+		return shaderModule;
 	}
 
 	//Check if a device has all the required capabilities (is a discrete GPU and supports geometry shading).
@@ -613,6 +636,26 @@ private:
 		}
 
 		return extensions;
+	}
+
+	//Read file to char vector
+	static std::vector<char> readFile(const std::string& filename)
+	{
+		std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+		if(!file.is_open())
+		{
+			throw std::runtime_error("Failed to open file!");
+		}
+
+		size_t fileSize = (size_t)file.tellg();
+		std::vector<char> buffer(fileSize);
+
+		file.seekg(0);
+		file.read(buffer.data(), fileSize);
+		file.close();
+
+		return buffer;
 	}
 
 	//Check if validation layers are supported
