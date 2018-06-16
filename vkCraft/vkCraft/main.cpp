35fb3c844bfc61bab1736ddf78bc0c42894f2762
Tentone <tentone@outlook.com>
2018-06-16 17:43:33 +0100

    Indexing vertices

--- vkCraft/vkCraft/main.cpp
+++ vkCraft/vkCraft/main.cpp
@@ -56,11 +56,15 @@ struct Vertex
 
 //Vertex data
 const std::vector<Vertex> vertices = {
-	{ { 0.0f, -0.5f },{ 1.0f, 1.0f, 0.0f } },
-	{ { 0.5f, 0.5f },{ 0.0f, 1.0f, 0.0f } },
-	{ { -0.5f, 0.5f },{ 0.0f, 0.0f, 1.0f } }
+	{ { -0.5f, -0.5f },{ 1.0f, 0.0f, 0.0f } },
+	{ { 0.5f, -0.5f },{ 0.0f, 1.0f, 0.0f } },
+	{ { 0.5f, 0.5f },{ 0.0f, 0.0f, 1.0f } },
+	{ { -0.5f, 0.5f },{ 1.0f, 1.0f, 1.0f } }
 };
 
+const std::vector<uint16_t> indices = {
+	0, 1, 2, 2, 3, 0
+};
 
 const std::vector<const char*> validationLayers =
 {
@@ -159,6 +163,8 @@ private:
 	//Vertex data
 	VkBuffer vertexBuffer;
 	VkDeviceMemory vertexBufferMemory;
+	VkBuffer indexBuffer;
+	VkDeviceMemory indexBufferMemory;
 
 	//Syncronization semaphores (one for each concurrent frame)
 	std::vector<VkSemaphore> imageAvailableSemaphores;
@@ -209,6 +215,7 @@ private:
 		//Command pool
 		createCommandPool();
 		createVertexBuffer();
+		createIndexBuffer();
 		createCommandBuffers();
 
 		//Semaphores
@@ -264,10 +271,15 @@ private:
 		//Wait for the device to idle before cleaning up
 		vkDeviceWaitIdle(device);
 		
+		//Clean swap chain
 		cleanupSwapChain();
-
+		
+		//Buffers
 		vkDestroyBuffer(device, vertexBuffer, nullptr);
 		vkFreeMemory(device, vertexBufferMemory, nullptr);
+		
+		vkDestroyBuffer(device, indexBuffer, nullptr);
+		vkFreeMemory(device, indexBufferMemory, nullptr);
 
 		//Semaphores
 		for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++)
@@ -801,6 +813,28 @@ private:
 		vkFreeMemory(device, stagingBufferMemory, nullptr);
 	}
 
+	//Create index buffer
+	void createIndexBuffer()
+	{
+		VkDeviceSize bufferSize = sizeof(indices[0]) * indices.size();
+
+		VkBuffer stagingBuffer;
+		VkDeviceMemory stagingBufferMemory;
+		createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
+
+		void* data;
+		vkMapMemory(device, stagingBufferMemory, 0, bufferSize, 0, &data);
+		memcpy(data, indices.data(), (size_t)bufferSize);
+		vkUnmapMemory(device, stagingBufferMemory);
+
+		createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, indexBuffer, indexBufferMemory);
+
+		copyBuffer(stagingBuffer, indexBuffer, bufferSize);
+
+		vkDestroyBuffer(device, stagingBuffer, nullptr);
+		vkFreeMemory(device, stagingBufferMemory, nullptr);
+	}
+
 	//Copy the contents of a buffer to another buffer
 	void copyBuffer(VkBuffer srcBuffer, VkBuffer dstBuffer, VkDeviceSize size)
 	{
@@ -940,7 +974,9 @@ private:
 			VkBuffer vertexBuffers[] = {vertexBuffer};
 			VkDeviceSize offsets[] = {0};
 			vkCmdBindVertexBuffers(commandBuffers[i], 0, 1, vertexBuffers, offsets);
-			vkCmdDraw(commandBuffers[i], static_cast<uint32_t>(vertices.size()), 1, 0, 0);
+			vkCmdBindIndexBuffer(commandBuffers[i], indexBuffer, 0, VK_INDEX_TYPE_UINT16);
+
+			vkCmdDrawIndexed(commandBuffers[i], static_cast<uint32_t>(indices.size()), 1, 0, 0, 0);
 
 			//End rendering
 			vkCmdEndRenderPass(commandBuffers[i]);
