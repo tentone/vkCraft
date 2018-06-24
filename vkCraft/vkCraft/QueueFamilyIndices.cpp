20094de5264b5c80e4ea85e182f360f6b28e932b
Tentone <tentone@outlook.com>
2018-06-24 16:28:34 +0100

    Organizing code

---
+++ vkCraft/vkCraft/QueueFamilyIndices.cpp
@@ -0,0 +1,13 @@
+#pragma once
+
+class QueueFamilyIndices
+{
+public:
+	int graphicsFamily = -1;
+	int presentFamily = -1;
+
+	bool isComplete()
+	{
+		return graphicsFamily >= 0 && presentFamily >= 0;
+	}
+};
