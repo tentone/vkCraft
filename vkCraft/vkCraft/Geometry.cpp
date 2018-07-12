4f0017946f84e0c46be9b02f81e1e9bf4a6c6257
Tentone <tentone@outlook.com>
2018-07-12 22:27:07 +0100

    Clean up

--- vkCraft/vkCraft/Geometry.cpp
+++ vkCraft/vkCraft/Geometry.cpp
@@ -25,6 +25,18 @@ public:
 		//TODO <ADD CODE HERE>
 	}
 	
+	//Apply a matrix transformation to the geometry vertex position
+	void applyTransformationMatrix(glm::mat4 matrix)
+	{
+		//TODO <ADD CODE HERE>
+	}
+
+	//Append another geometry data to this geometry
+	void append(Geometry *geometry)
+	{
+		//TODO <ADD CODE HERE>
+	}
+
 	//Dispose the geometry
 	void dispose(VkDevice device)
 	{
